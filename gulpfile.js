bcd9ad5d2f9a91402c5f9013ab5e384ddbba1cac
Zachetka <axi83@mail.ru>
2021-02-27 15:23:04 +0300

    add section trends

--- gulpfile.js
+++ gulpfile.js
@@ -50,12 +50,16 @@ const path = {
         favicons: `${destPath}/assets/images/favicons`,
         font: `${destPath}/assets/fonts`,
     },
-    styleLibs: [
+    styleLibs: [        
+        'node_modules/rateyo/src/jquery.rateyo.css',
+        'node_modules/slick-carousel/slick/slick.css'
         
     ],
     scriptLibs: [
-        'node_modules/jquery/dist/jquery.js',
-        'node_modules/mixitup/dist/mixitup.js'
+        'node_modules/jquery/dist/jquery.js',        
+        'node_modules/rateyo/src/jquery.rateyo.js',
+        'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/slick-carousel/slick/slick.js'
     ],
 };
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,25 @@
 $(function () {
   var containerEl = document.querySelector('.products__cards');
-
   var mixer = mixitup(containerEl, {
     animation: {
       effects: 'fade scale(0.5)',
     },
+  });
+
+  $('.card__rating-stars').rateYo({
+    rating: 4.5,
+    starWidth: '12px',
+    spacing: '2px',
+    normalFill: "#546e7a",
+    ratedFill: "#ffa726",
+    readOnly: true
+  });
+
+  $('.trends__slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    arrows: false,
+    dots: true,
   });
 });
